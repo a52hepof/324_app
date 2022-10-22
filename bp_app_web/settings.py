6c8723506f097b02da4a631ec33ff2aa5e93c789
Fernando <a52hepof@uco.es>
2022-10-22 22:56:23 +0200

    debug mode

--- bp_app_web/settings.py
+++ bp_app_web/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-#DEBUG = 'RENDER' not in os.environ ## si no existe devuelve True
+DEBUG = 'RENDER' not in os.environ ## si no existe devuelve True
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
