ec5a708db86c13e4023b7ccf19f0d2f4d096511c
Fernando <a52hepof@uco.es>
2022-10-23 23:40:56 +0200

    conexiones db test

--- AccessControl/tests.py
+++ AccessControl/tests.py
@@ -3,10 +3,22 @@ import psycopg2
 # Create your tests here.
 print('Probando conexiones remotas')
 
+
+
+
+try:
+    conexion = psycopg2.connect(host="ec2-52-208-164-5.eu-west-1.compute.amazonaws.com", database="d2i565903pvam0", user="rzmdeawngownsi", password="e0f4dacf6ae38caa35190a376bd295478400b869fa5869ec4d0c33c229cd0f64")
+    cur = conexion.cursor()
+    print(conexion,cur)
+except Exception as e:
+    print(e)
+
+
+
 try:
     conexion = psycopg2.connect(host="tommy2.heliohost.org", database="a52hepof_bp324_db", user="a52hepof_bp324_user", password="U74&a93iu")
     cur = conexion.cursor()
-    print(cur)
+    print('Helios',cur)
 except Exception as e:
     print(e)
 
@@ -26,6 +38,6 @@ except Exception as e:
 try:
     conexion = psycopg2.connect(host="database-324-app.caqpfa3nnout.us-east-1.rds.amazonaws.com", database="database324app", user="postgres324", password="U74&a93iu")
     cur = conexion.cursor()
-    print(cur)
+    print(conexion,cur)
 except Exception as e:
     print(e)
